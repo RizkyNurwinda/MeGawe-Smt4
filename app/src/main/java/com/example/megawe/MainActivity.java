81f1fa5263bc28ec342ad42f70386ac5acfedaee
anisa231299 <anisasaf5@gmail.com>
2020-04-30 11:05:55 +0700

    HOME

---
+++ app/src/main/java/com/example/megawe/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.megawe;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        loadFragment (new HomeFragment());
+        BottomNavigationView bottomNav = findViewById(R.id.menu_bawah);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.cari:
+                            selectedFragment = new CariFragment();
+                            break;
+                        case R.id.like:
+                            selectedFragment = new LikeFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+                    //return false;
+                    return true;
+                }
+            };
+}
