6313067ce0cc450c1c21b3f810d3d0c2fb8827bf
anisa231299 <anisasaf5@gmail.com>
2020-05-08 22:00:45 +0700

    8-05

--- app/src/main/java/com/example/megawe/AfterLoginActivity.java
+++ app/src/main/java/com/example/megawe/AfterLoginActivity.java
@@ -2,13 +2,48 @@ package com.example.megawe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class AfterLoginActivity extends AppCompatActivity {
+public class AfterLoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button TambahPekerjaan, Riwayat, Profil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_after_login);
+
+        TambahPekerjaan=(Button) findViewById(R.id.TambahPekerjaan);
+        TambahPekerjaan.setOnClickListener(this);
+
+        Riwayat=(Button) findViewById(R.id.Riwayat);
+        Riwayat.setOnClickListener(this);
+
+        Profil=(Button) findViewById(R.id.Profil);
+        Profil.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.TambahPekerjaan:
+                Intent rekomendasi =new Intent(AfterLoginActivity.this, AddPekerjaanActivity.class);
+                startActivity(rekomendasi);
+                break;
+            case R.id.Riwayat:
+                Intent pesan =new Intent(AfterLoginActivity.this, RiwayatActivity.class);
+                startActivity(pesan);
+                break;
+            case R.id.Profil:
+                Intent add= new Intent(AfterLoginActivity.this, ProfilActivity.class);
+                startActivity(add);
+                break;
+
+        }
     }
 }
+
--- app/src/main/java/com/example/megawe/RiwayatActivity.java
+++ app/src/main/java/com/example/megawe/RiwayatActivity.java
@@ -2,9 +2,19 @@ package com.example.megawe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.SearchView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 public class RiwayatActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
