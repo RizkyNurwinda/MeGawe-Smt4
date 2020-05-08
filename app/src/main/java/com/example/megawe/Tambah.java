1812c11ac2fb12b2825698b7bc96c294474376ad
nina1103 <nina.ardana87@gmail.com>
2020-05-07 22:33:54 -0700

    tambah

---
+++ app/src/main/java/com/example/megawe/Tambah.java
@@ -0,0 +1,17 @@
+package com.example.megawe;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Tambah extends AppCompatActivity {
+
+    Button TambahPekerjaan,Riwayat,Profil;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tambah);
+
+    }
+}
